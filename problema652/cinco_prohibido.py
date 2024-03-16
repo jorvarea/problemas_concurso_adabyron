fc731d2bbf94694a2ee888707e2d16ab703d5ad7
jorvarea <jorvarea@users.noreply.github.com>
2024-03-16 16:18:56 +0100

    version base 9

--- problema652/cinco_prohibido.py
+++ problema652/cinco_prohibido.py
@@ -1,6 +1,9 @@
-def calcular_multiplicacion(num1: int, num2: int) -> int:
-    resultado_normal = num1 * num2
-    resultado_sin_cinco = resultado_normal + (resultado_normal // 5) - (resultado_normal // 10)
+def calcular_multiplicacion(num1: int, num2: int) -> str:
+    resultado_decimal = num1 * num2
+    resultado_sin_cinco = ""
+    while resultado_decimal > 0:
+        resultado_sin_cinco += "012346789"[resultado_decimal % 9]
+        resultado_decimal //= 9
     return resultado_sin_cinco
 
 def procesar_datos() -> None:
